7d6fc54175ed38c659d3a371d330f4aae809d81d
Shantanu Acharya <thegeek.004@gmail.com>
2018-04-24 05:20:44 +0530

    Add Transaction Model

--- market/admin.py
+++ market/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Company
+from .models import Company, Transaction
 
 
 admin.site.register(Company)
+admin.site.register(Transaction)
--- market/models.py
+++ market/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+TRANSACTION_MODES = (
+    ('buy', 'BUY'),
+    ('sell', 'SELL')
+)
 
 
 class Company(models.Model):
@@ -13,3 +22,18 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User)
+    company = models.ForeignKey(Company)
+    num_stocks = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
+    mode = models.CharField(max_length=10, choices=TRANSACTION_MODES)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{user}: {company} - {time}'.format(
+            user=self.user.username, company=self.company.name, time=self.timestamp
+        )
